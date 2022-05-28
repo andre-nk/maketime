6903285a914bbc4a8515464b22fa656f5b3de147
Andreas Notokuusmo <andreasnotokusumo23@gmail.com>
2022-05-28 16:41:32 +0700

    :recycle: Project Refactor and HyperUI Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,57 @@
-import React, { ReactElement, useState } from 'react'
-import logo from './logo.svg'
+import { ReactElement, useState } from "react";
 
 function App(): ReactElement {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="border border-gray-50 rounded-xl p-20 shadow-xl">
+    <div className="w-full h-full flex flex-col justify-start">
       <header>
-        <div className="flex justify-center">
-          <img src={logo} className="h-32 w-32 animate-spin-slow" alt="logo" />
+        <div className="w-full px-4 py-8 sm:px-6 lg:px-8">
+          <div className="sm:justify-between sm:items-center sm:flex">
+            <div className="text-center sm:text-left">
+              <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+                Welcome Back, Barry!
+              </h1>
+
+              <p className="mt-1.5 text-sm text-gray-500">
+                Let's write a new blog post! 🎉
+              </p>
+            </div>
+            <div className="flex flex-col gap-4 mt-4 sm:flex-row sm:mt-0 sm:items-center">
+              <button
+                className="inline-flex items-center justify-center px-5 py-3 text-gray-500 transition border border-gray-200 rounded-lg hover:text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring"
+                type="button"
+              >
+                <span className="text-sm font-medium"> View Website </span>
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-4 h-4 ml-1.5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                  />
+                </svg>
+              </button>
+
+              <button
+                className="block px-5 py-3 text-sm font-medium text-white transition bg-indigo-600 rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring"
+                type="button"
+              >
+                Create Post
+              </button>
+            </div>
+          </div>
         </div>
-        <p className="text-2xl pb-3">Hello Vite + React + tailwindcss!</p>
-        <p>
-          <button
-            className="bg-purple-400 pl-2 pr-2 pt-1 pb-1 rounded text-sm text-purple-100"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            count is: {count}
-          </button>
-        </p>
-        <p className="pb-3 pt-3">
-          Edit{' '}
-          <code className="border border-1 pl-1 pr-1 pb-0.5 pt-0.5 rounded border-purple-400 font-mono text-sm bg-purple-100 text-purple-900">
-            src/App.tsx
-          </code>{' '}
-          and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="text-purple-400 underline"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="text-purple-400 underline"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-          {' | '}
-          <a
-            className="text-purple-400 underline"
-            href="https://tailwindcss.com/docs"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            tailwindcss Docs
-          </a>
-        </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
